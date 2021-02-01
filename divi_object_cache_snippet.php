b2ec05ebd2a43f326a3cbee43ab4952d5e27116f
FreshyMichael <michael@freshysites.com>
2021-02-01 13:02:45 -0500

    Update divi_object_cache_snippet.php

--- divi_object_cache_snippet.php
+++ divi_object_cache_snippet.php
@@ -1,4 +1,5 @@
 <?php 
+add_action('after_setup_theme','divi_static_css_404_override');
 
 function divi_static_css_404_override (){
 
@@ -7,5 +8,4 @@ if ( is_object($advanced_post_cache_object->is_404) ) {
     $advanced_post_cache_object->flush_cache();
 }
 
-  if (     )
 }
